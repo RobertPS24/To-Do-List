4a57d62c521909182e9dbc499da57ae442de2b6d
Popescu Robert <64865419+RobertPS24@users.noreply.github.com>
2025-02-27 19:46:35 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ToDoListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTask = new TextBox();
+            btnAddTask = new Button();
+            btnRemoveTask = new Button();
+            btnCompleteTask = new Button();
+            btnSave = new Button();
+            btnLoad = new Button();
+            lstTasks = new ListBox();
+            SuspendLayout();
+            // 
+            // txtTask
+            // 
+            txtTask.Location = new Point(276, 31);
+            txtTask.Name = "txtTask";
+            txtTask.Size = new Size(125, 27);
+            txtTask.TabIndex = 0;
+            txtTask.TextChanged += textBox1_TextChanged;
+            // 
+            // btnAddTask
+            // 
+            btnAddTask.Location = new Point(295, 86);
+            btnAddTask.Name = "btnAddTask";
+            btnAddTask.Size = new Size(94, 29);
+            btnAddTask.TabIndex = 1;
+            btnAddTask.Text = "Add Task";
+            btnAddTask.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveTask
+            // 
+            btnRemoveTask.Location = new Point(172, 254);
+            btnRemoveTask.Name = "btnRemoveTask";
+            btnRemoveTask.Size = new Size(165, 29);
+            btnRemoveTask.TabIndex = 2;
+            btnRemoveTask.Text = "Remove Task";
+            btnRemoveTask.UseVisualStyleBackColor = true;
+            // 
+            // btnCompleteTask
+            // 
+            btnCompleteTask.Location = new Point(335, 254);
+            btnCompleteTask.Name = "btnCompleteTask";
+            btnCompleteTask.Size = new Size(164, 29);
+            btnCompleteTask.TabIndex = 3;
+            btnCompleteTask.Text = "Complete Task";
+            btnCompleteTask.UseVisualStyleBackColor = true;
+            btnCompleteTask.Click += button3_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(243, 371);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(345, 371);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(94, 29);
+            btnLoad.TabIndex = 5;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // lstTasks
+            // 
+            lstTasks.FormattingEnabled = true;
+            lstTasks.Location = new Point(172, 130);
+            lstTasks.Name = "lstTasks";
+            lstTasks.Size = new Size(327, 104);
+            lstTasks.TabIndex = 6;
+            lstTasks.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(700, 450);
+            Controls.Add(lstTasks);
+            Controls.Add(btnLoad);
+            Controls.Add(btnSave);
+            Controls.Add(btnCompleteTask);
+            Controls.Add(btnRemoveTask);
+            Controls.Add(btnAddTask);
+            Controls.Add(txtTask);
+            Name = "Form1";
+            Text = "ToDoList";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTask;
+        private Button btnAddTask;
+        private Button btnRemoveTask;
+        private Button btnCompleteTask;
+        private Button btnSave;
+        private Button btnLoad;
+        private ListBox lstTasks;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System.IO;
+using System.Windows.Forms;
+namespace ToDoListApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnAddTask.Click += btnAddTask_Click;
+            btnRemoveTask.Click += btnRemoveTask_Click;
+            btnCompleteTask.Click += btnCompleteTask_Click;
+            btnSave.Click += btnSave_Click;
+            btnLoad.Click += btnLoad_Click;
+
+            this.BackColor = Color.LightGray;
+
+            btnAddTask.BackColor = Color.Blue;
+            btnAddTask.ForeColor = Color.White;
+
+            btnRemoveTask.BackColor = Color.Red;
+            btnRemoveTask.ForeColor = Color.White;
+
+            btnCompleteTask.BackColor = Color.Green;
+            btnCompleteTask.ForeColor = Color.White;
+
+            btnSave.BackColor = Color.Orange;
+            btnSave.ForeColor = Color.Black;
+
+            btnLoad.BackColor = Color.Purple;
+            btnLoad.ForeColor = Color.White;
+
+
+            lstTasks.BackColor = Color.Beige;
+            lstTasks.ForeColor = Color.Black;
+
+
+
+            lstTasks.HorizontalScrollbar = false;
+            lstTasks.HorizontalExtent = 0;
+
+
+
+
+
+        }
+
+
+        //
+        private void UpdateHorizontalScroll()
+        {
+            int maxWidth = 0;
+            using (Graphics g = lstTasks.CreateGraphics())
+            {
+                foreach (var item in lstTasks.Items)
+                {
+                    int itemWidth = (int)g.MeasureString(item.ToString(), lstTasks.Font).Width;
+                    if (itemWidth > maxWidth)
+                        maxWidth = itemWidth;
+                }
+            }
+
+
+            if (maxWidth > lstTasks.ClientSize.Width)
+            {
+                lstTasks.HorizontalScrollbar = true;
+                lstTasks.HorizontalExtent = maxWidth;
+            }
+            else
+            {
+                lstTasks.HorizontalScrollbar = false;
+            }
+        }
+
+        //
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                lstTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+                UpdateHorizontalScroll();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedItem != null)
+            {
+                lstTasks.Items.Remove(lstTasks.SelectedItem);
+                UpdateHorizontalScroll();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to remove!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCompleteTask_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedItem != null)
+            {
+                int index = lstTasks.SelectedIndex;
+                string task = lstTasks.SelectedItem.ToString();
+
+                if (!task.StartsWith("[✔] "))
+                {
+                    lstTasks.Items[index] = "[✔] " + task;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to mark as complete!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("tasks.txt"))
+            {
+                foreach (var item in lstTasks.Items)
+                {
+                    writer.WriteLine(item.ToString());
+                }
+            }
+            MessageBox.Show("Tasks saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("tasks.txt"))
+            {
+                lstTasks.Items.Clear();
+                using (StreamReader reader = new StreamReader("tasks.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lstTasks.Items.Add(line);
+                    }
+                }
+                MessageBox.Show("Tasks loaded successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No saved tasks found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
